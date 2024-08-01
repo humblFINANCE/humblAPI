2ccfffd8a5d4cd507df758da7fb2285b81a507a1
jenningsFantini <jenningsfantini+github@gmail.com>
2024-08-01 13:51:16 +0100

    ♻️ refactor(core): add root paths and health-checks to their own router

--- src/humblapi/main.py
+++ src/humblapi/main.py
@@ -14,11 +14,10 @@ from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
 from starlette.middleware.base import BaseHTTPMiddleware
 
-from humblapi.api.v1.routers import openbb, portfolio, toolbox
+from humblapi.api.v1.routers import openbb, portfolio, toolbox, core
 from humblapi.core.config import config
 from humblapi.core.env import Env
 from humblapi.core.middleware import TimeLogMiddleware
-from humblapi.core.utils import raise_http_exception, redis_delete_pattern
 
 env = Env()
 
@@ -72,109 +71,4 @@ app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.include_router(portfolio.router)
 app.include_router(toolbox.router)
 app.include_router(openbb.router)
-
-
-@app.get("/")
-@cache(expire=60)
-async def read_root() -> dict:
-    """Read root."""
-    return {
-        "data": {
-            "message": "Welcome to the humblapi API",
-        }
-    }
-
-
-@app.get("/health")
-@cache(expire=60)
-async def health_check():
-    """
-    Health check endpoint.
-
-    Returns
-    -------
-        dict: A JSON response indicating the API's health status and HTTP status code.
-    """
-    return {"data": {"message": "API is healthy", "status": 200}}
-
-
-@app.get("/redis-health")
-@cache(expire=10)
-async def redis_health_check():
-    """
-    Check the health of the Redis connection.
-
-    Returns
-    -------
-        dict: A JSON response indicating the Redis connection status and HTTP status code.
-    """
-    try:
-        redis_backend = FastAPICache.get_backend()
-        if isinstance(redis_backend, RedisBackend):
-            redis = redis_backend.redis
-            if await redis.ping():
-                return {"data": {"message": "PONG", "status_code": 200}}
-        raise_http_exception(500, "Redis connection failed")
-    except Exception as e:
-        raise_http_exception(500, f"Redis connection error: {e!s}")
-
-
-@app.get("/flush-redis")
-async def flush_redis(
-    token: str = Query(description="Secret API token for flushing Redis"),
-    fastapi_cache_only: bool = Query(
-        description="If true, ONLY flush the FastAPI cache. This removes all keys with the 'fastapi-cache:' prefix.",
-        default=False,
-    ),
-):
-    """
-    Flush the Redis database or only the FastAPI cache.
-
-    Parameters
-    ----------
-    token : str
-        Secret API token for authentication.
-    fastapi_cache_only : bool
-        If true, only flush the FastAPI cache.
-
-    Returns
-    -------
-    dict
-        A dictionary containing a success message and status code.
-
-    Raises
-    ------
-    HTTPException
-        If the token is invalid or if there's an error flushing Redis.
-    """
-    if token != config.FLUSH_API_TOKEN:
-        raise_http_exception(403, "Invalid API token")
-
-    try:
-        redis_backend = FastAPICache.get_backend()
-        if isinstance(redis_backend, RedisBackend):
-            redis = redis_backend.redis
-            if fastapi_cache_only:
-                records_deleted = await redis_delete_pattern(
-                    redis, "fastapi-cache:*"
-                )
-                return {
-                    "data": {
-                        "message": "FastAPI cache was flushed successfully.",
-                        "status_code": 200,
-                        "records_deleted": records_deleted,
-                    }
-                }
-            else:
-                records = await redis.dbsize()
-                await redis.flushdb()
-                return {
-                    "data": {
-                        "message": "Redis database flushed successfully.",
-                        "status_code": 200,
-                        "records_deleted": records,
-                    }
-                }
-        raise_http_exception(500, "Redis backend not found")
-    except Exception as e:
-        raise_http_exception(500, f"Error flushing Redis: {e!s}")
+app.include_router(core.router)
