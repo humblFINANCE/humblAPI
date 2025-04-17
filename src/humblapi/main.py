21c24513af75bcc43dfd92c196ce3d05e2d25576
jjfantini <jenningsfantini+github@gmail.com>
2025-04-17 04:45:43 +0000

    🔖 bump(release): v0.16.3 → v0.16.4

--- src/humblapi/main.py
+++ src/humblapi/main.py
@@ -83,7 +83,7 @@ app = FastAPI(
     title=config.PROJECT_NAME,
     lifespan=lifespan,
     default_response_class=ORJSONResponse,
-    version="0.16.3",
+    version="0.16.4",
 )
 
 # Add Middleware
